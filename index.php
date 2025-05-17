97597162d93e5eef6e053da2f7813fbf83e09871
quratulain-123 <149178975+quratulain-123@users.noreply.github.com>
2025-05-17 21:30:57 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Grid System</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container-fluid">
+    <div class="row">
+        <div class="col-12">
+        <nav class="navbar navbar-expand-lg bg-danger-subtle my-3 p-3 rounded">
+       <div class="container">
+         <a class="navbar-brand bg-" href="#">Ashion</a>
+         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+           <span class="navbar-toggler-icon"></span>
+         </button>
+         <div class="collapse navbar-collapse" id="navbarNav">
+           <ul class="navbar-nav">
+             <li class="nav-item">
+               <a class="nav-link active" aria-current="page" href="#">Home</a>
+             </li>
+             <li class="nav-item">
+               <a class="nav-link" href="#">Shop</a>
+             </li>
+             <li class="nav-item">
+               <a class="nav-link" href="#">Blog</a>
+             </li>
+             <li class="nav-item">
+               <a class="nav-link" href="#">Contact</a>
+             </li>
+           </ul>
+         </div>
+       </div>
+       </nav>
+    </div>             
+        </div>
+        <div class="row">
+             <div class="col-6">
+             <div class="card"  >
+               <img src="images/1.jpg" class="card-img-top" alt="...">
+               <h2 class="p-2">Women's</h2>
+               <div class="card-body">
+                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+               </div>
+             </div>
+             </div>
+             <div class="col-6">
+             <div class="card">
+              <img src="images/2.jpg" class="card-img-top" alt="...">
+              <h2 class="p-2">Men's</h2>
+              <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+             </div>
+        </div>
+        <div class="row">
+        <h2 class="my-3 rounded-pill text-center p-3">Category</h2>
+        <div class="row">
+         <div class="col-4 border border-1 border-primary-subtle rounded">
+            <h2>Kid's Fashion</h2>
+            <img class="img-fluid rounded" src="images/3.png" alt="">
+            <a href="#" class="btn btn-info m-3">Explore</a>
+         </div>
+         <div class="col-4 border border-1 border-primary-subtle rounded">
+         <h2>Accessories</h2>
+         <img class="img-fluid rounded" src="images/4.png" alt="">
+         <a href="#" class="btn btn-info m-3">Explore</a>
+         </div>
+         <div class="col-4 border border-1 border-primary-subtle rounded">
+         <h2>Cosmetics</h2>
+         <img class="img-fluid rounded" src="images/5.png" alt="">
+         <a href="#" class="btn btn-info m-3">Explore</a>
+         </div>
+         </div>
+      </div>
+      <div class="row">
+        <h2 class="text-center fw-bold my-3 p-2 ">New Arraival</h2>
+        <div class="col-3">
+            <img src="images/6.png" style="width: 250px;" class="rounded">
+            <h5>Winter Jacket</h4>
+            <p class="text-secondary">$10.76</p>
+        </div>
+        <div class="col-3">
+            <img src="images/7.png" style="width: 280px;" class="rounded">
+            <h5>Wedding Gaon</h4>
+            <p class="text-secondary">$100</p>
+        </div>
+        <div class="col-3">
+            <img src="images/8.png" style="width: 250px;" class="rounded">
+            <h5>Long chain</h4>
+            <p class="text-secondary">$50</p>
+        </div>
+        <div class="col-3">
+            <img src="images/9.png" style="width: 250px;" class="rounded">
+            <h5>Sneaker</h4>
+            <p class="text-secondary">$20</p>
+        </div>
+      </div>
+      <div class="row">
+      <h2 class="text-center fw-bold my-3 p-2 ">Our Services</h2>
+        <div class="col-4">
+         <div class="card mb-3" style="width: 20rem;">
+         <div class="card-body">
+         <h5 class="card-title">Free Shipping</h5>
+         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+         <a href="#" class="btn btn-primary">Read more</a>
+         </div>
+         </div>
+        </div>
+        <div class="col-4">
+        <div class="card mb-3" style="width: 20rem;">
+         <div class="card-body">
+         <h5 class="card-title">Return Policy</h5>
+         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+         <a href="#" class="btn btn-primary">Read more</a>
+         </div>
+         </div>
+        </div>
+        <div class="col-4">
+        <div class="card mb-3" style="width: 20rem;">
+         <div class="card-body">
+         <h5 class="card-title">100% Satisfaction Guarantee</h5>
+         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+         <a href="#" class="btn btn-primary">Read more</a>
+         </div>
+         </div>
+        </div>
+        <div class="col-4">
+        <div class="card mb-3" style="width: 20rem;">
+         <div class="card-body">
+         <h5 class="card-title">100% Payment Secure</h5>
+         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+         <a href="#" class="btn btn-primary">Read more</a>
+         </div>
+         </div>
+        </div>
+        <div class="col-4">
+        <div class="card mb-3" style="width: 20rem;">
+         <div class="card-body">
+         <h5 class="card-title">Affordable Price</h5>
+         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+         <a href="#" class="btn btn-primary">Read more</a>
+         </div>
+         </div>
+        </div>
+        <div class="col-4">
+        <div class="card mb-3" style="width: 20rem;">
+         <div class="card-body">
+         <h5 class="card-title">Customer Support</h5>
+         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+         <a href="#" class="btn btn-primary">Read more</a>
+         </div>
+         </div>
+        </div>
+      </div>
+    </div>
+
+ <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
